bcce016571cd2641888a8d96fe1bdc9a59ad8f98
Satake <satake00157@gmail.com>
2021-03-13 02:37:10 +0900

    change context.subscriptions.push(disposable) to client cuz for deactive

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -8,40 +8,62 @@
  * ------------------------------------------------------------------------------------------ */
 "use strict";
 
-import * as path from 'path';
-import { workspace, Disposable, ExtensionContext } from 'vscode';
-import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind } from 'vscode-languageclient';
+import * as path from "path";
+import { workspace, ExtensionContext } from "vscode";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
+  // The server is implemented in node
+  let serverModule = context.asAbsolutePath(path.join("server", "server.js"));
+  // The debug options for the server
+  let debugOptions = { execArgv: ["--nolazy", "--debug=6004"] };
 
-    // The server is implemented in node
-    let serverModule = context.asAbsolutePath(path.join("server", "server.js"));
-    // The debug options for the server
-    let debugOptions = { execArgv: ["--nolazy", "--debug=6004"] };
+  // If the extension is launch in debug mode the debug server options are use
+  // Otherwise the run options are used
+  let serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions,
+    },
+  };
 
-    // If the extension is launch in debug mode the debug server options are use
-    // Otherwise the run options are used
-    let serverOptions: ServerOptions = {
-        run : { module: serverModule, transport: TransportKind.ipc },
-        debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
-    };
+  // Options to control the language client
+  let clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: ["plaintext", "markdown"], // TODO: add markdown
+    synchronize: {
+      // Synchronize the setting section 'redpen' to the server
+      configurationSection: "redpen",
+      // Notify the server about file changes to '.clientrc files contain in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/redpen-conf-*.xml"),
+    },
+  };
 
-    // Options to control the language client
-    let clientOptions: LanguageClientOptions = {
-        // Register the server for plain text documents
-        documentSelector: ["plaintext", "markdown"], // TODO: add markdown
-        synchronize: {
-            // Synchronize the setting section 'redpen' to the server
-            configurationSection: "redpen",
-            // Notify the server about file changes to '.clientrc files contain in the workspace
-            fileEvents: workspace.createFileSystemWatcher("**/redpen-conf-*.xml")
-        }
-    };
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    "Redpen",
+    serverOptions,
+    clientOptions
+  )
+  client.start();
 
-    // Create the language client and start the client.
-    let disposable = new LanguageClient("Redpen", serverOptions, clientOptions).start();
+  // Push the disposable to the context's subscriptions so that the
+  // client can be deactivated on extension deactivation
+  // context.subscriptions.push(disposable);
+}
 
-    // Push the disposable to the context's subscriptions so that the 
-    // client can be deactivated on extension deactivation
-    context.subscriptions.push(disposable);
+export function deactivated(): Thenable<void> | undefined {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
 }
